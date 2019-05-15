fe17d114b1ec8e4a31143776bbe73f173e93cf5a
MaxRobertsDear <maxhroberts@gmail.com>
2019-05-15 11:34:01 +0100

    refactored test and functional code for note list view module

--- spec/note-list-viewSpec.js
+++ spec/note-list-viewSpec.js
@@ -1,11 +1,14 @@
+
+
 function testNoteListView() {
     // arrange
-    var noteListView = new NoteListView(noteList = new NoteList);
+    var noteList = new NoteList();
     var note = new Note("here you go");
-    var note2 = new Note("no thanks")
-
+    var note2 = new Note("no thanks");
+    
     //act 
     noteList.array.push(note, note2)
+    var noteListView = new NoteListView(noteList);
     
     //assert
     assert.isTrue(noteListView.getHTML() === '<ul><li><div>here you go</div></li><li><div>no thanks</div></li></ul>');
--- src/note-list-view.js
+++ src/note-list-view.js
@@ -3,7 +3,7 @@
         this.noteList = noteList
     };
     NoteListView.prototype.getHTML = function() {
-        var whatever = noteList.array.map(i => i.getText())
+        var whatever = this.noteList.array.map(i => i.getText())
         var htmlJoined = whatever.join('</div></li><li><div>')
         var htmlOutput = "<ul><li><div>" + htmlJoined + "</div></li></ul>"
         return htmlOutput;
